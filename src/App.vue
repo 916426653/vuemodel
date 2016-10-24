b89908f02999adbc968c87a1b942a74cf715a9fe
916426653 <18317773923@163.com>
2016-10-24 14:44:05 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <h1>姓名：{{name}}</h1>
+    <h2>{{age}}</h2>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 export default {
   name: 'app',
-  components: {
-    Hello
+  data () {
+    return {
+      'name': 'wqq',
+      'age': '21'
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
+  /*一定要加lang不然无法编译*/
+  /*测试一下对sass的编译*/
+
+
+
+  h1{
+    background-color: #eee;
+    color: red;
+    transform: translate(10%, 10%);/*测试自动添加前缀*/
+  }
+  h1:hover{
+    height:100px;
+  }
+  h2{
+    background-color: #999;
+  }
+
 </style>
+
+
+
+
